bf13cf29e660cf93fab89733b772a87a19aebcd8
Spencer Richey <richeyslr@gmail.com>
2021-08-09 13:36:59 -0700

    saving work. almost done w functionality

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// declare variables needed for universal functionality
+let maxClicks = 10;
+let totalClicks = 0;
+let leftProduct = null;
+let middleProduct = null;
+let rightProduct = null;
+
 // make a class for new product object
 class ProductObj {
   clicks = 0;
@@ -23,7 +30,7 @@ let allProducts = [
   new ProductObj("Pen", "./product-images/assets/pen.jpg"),
   new ProductObj("Scissors", "./product-images/assets/scissors.jpg"),
   new ProductObj("Shark", "./product-images/assets/shark.jpg"),
-  new ProductObj("Sweep", "./product-images/assets/sweep.jpg"),
+  new ProductObj("Sweep", "./product-images/assets/sweep.png"),
   new ProductObj("Tauntaun", "./product-images/assets/tauntaun.jpg"),
   new ProductObj("Unicorn", "./product-images/assets/unicorn.jpg"),
   new ProductObj("Water Can", "./product-images/assets/water-can.jpg"),
@@ -39,8 +46,10 @@ const middleImgText = document.querySelector("#middleImgText");
 const rightImg = document.querySelector("#rightImg");
 const rightImgText = document.querySelector("#rightImgText");
 const totalsList = document.querySelector("#totalsList");
+const resultsButton = document.querySelector("#resultsButton");
 
-renderRandomProducts = function () {
+// create a function to randomly generate array Products and render them on page in designated spot
+let renderRandomProducts = function () {
   let duplicates = true;
   while (duplicates) {
     let leftProductIndex = Math.floor(Math.random() * allProducts.length);
@@ -52,6 +61,12 @@ renderRandomProducts = function () {
       middleProductIndex != rightProductIndex
     ) {
       duplicates = false;
+      leftProduct = allProducts[leftProductIndex];
+      middleProduct = allProducts[middleProductIndex];
+      rightProduct = allProducts[rightProductIndex];
+      allProducts[leftProductIndex].timesShown++;
+      allProducts[middleProductIndex].timesShown++;
+      allProducts[rightProductIndex].timesShown++;
       leftImg.src = allProducts[leftProductIndex].imgSrc;
       leftImgText.innerText = allProducts[leftProductIndex].name;
       middleImg.src = allProducts[middleProductIndex].imgSrc;
@@ -61,4 +76,37 @@ renderRandomProducts = function () {
     }
   }
 };
+
+// create a function that handles the event of an product being clicked
+let handleProductClick = function (evt) {
+  if (totalClicks <= maxClicks) {
+    totalClicks++;
+    if (evt.target == leftImg) {
+      leftProduct.clicks++;
+    } else if (evt.target == middleImg) {
+      middleProduct.clicks++;
+    } else if (evt.target == rightImg) {
+      rightProduct.clicks++;
+    }
+    renderRandomProducts();
+  } else {
+    renderProductData();
+  }
+};
+
+// create a function to render the results data to a list on the page
+let renderProductData = function () {
+  for (let i = 0; i < allProducts.length; i++) {
+    let newLi = document.createElement("li");
+    let listProductHead = document.createElement("h5");
+    listProductHead.innerText = allProducts[i].name;
+    newLi.innerText = `${listProductHead} was shown ${allProducts[i].timesShown} times and clicked ${allProducts[i].clicks} times`;
+    totalsList.appendChild(newLi);
+  }
+};
+
+// load page with random products
 renderRandomProducts();
+
+// create an event listener to handle clicks
+imageSection.addEventListener("click", handleProductClick);
