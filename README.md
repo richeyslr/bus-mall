00d049bc41686ceef7c33a6982b77d2c8aa8f5b6
Spencer Richey <richeyslr@gmail.com>
2021-08-10 13:07:24 -0700

    added chart. stack type doesnt work how I want it to work. made some small positioning changes.

--- README.md
+++ README.md
@@ -7,7 +7,15 @@ As a user, I would like to track the selections made by viewers so that I can de
 As a user, I would like to control the number of rounds a user is presented with so that I can control the voting session duration.
 As a user, I would like to view a report of results after all rounds of voting have concluded so that I can evaluate which products were the most popular.
 
+## Lab 12 - Add a chart
+
+Experiment with using Chart.js to implement a graph showing click data
+[Resources:](https://www.chartjs.org/docs/latest/samples/bar/stacked.html) Chart.js stacked bar chart example 
+
 To do:
-make images line up left to right
+prevent same product from showing up in back to back selections
 make button appear after voting is done
 make button render totals data
+make totals data render in table form instead of list form
+style bar chart to better represent data
+consolidate while loop to check for dupes
--- app.js
+++ app.js
@@ -36,9 +36,16 @@ let allProducts = [
   new ProductObj("Water Can", "./product-images/assets/water-can.jpg"),
   new ProductObj("Wine Glass", "./product-images/assets/wine-glass.jpg"),
 ];
+// make arrays to hold chart data
+let productLabels = [];
+let productClicks = [];
+let productTimesShown = [];
 
 // define names for DOM targets
 const imageSection = document.querySelector("#imageSection");
+const leftProductDiv = document.querySelector('#leftProductDiv');
+const middleProductDiv = document.querySelector('#middleProductDiv');
+const rightProductDiv = document.querySelector('#rightProductDiv');
 const leftImg = document.querySelector("#leftImg");
 const leftImgText = document.querySelector("#leftImgText");
 const middleImg = document.querySelector("#middleImg");
@@ -79,33 +86,88 @@ let renderRandomProducts = function () {
 
 // create a function that handles the event of an product being clicked
 let handleProductClick = function (evt) {
-  if (totalClicks <= maxClicks) {
+  if (totalClicks < maxClicks) {
     totalClicks++;
-    if (evt.target == leftImg) {
+    if (evt.target == leftProductDiv || evt.target == leftImg || evt.target == leftImgText) {
       leftProduct.clicks++;
-    } else if (evt.target == middleImg) {
+    } else if (evt.target == middleProductDiv || evt.target == middleImg || evt.target == middleImgText) {
       middleProduct.clicks++;
-    } else if (evt.target == rightImg) {
+    } else if (evt.target == rightProductDiv || evt.target == rightImg || evt.target == rightImgText) {
       rightProduct.clicks++;
     }
     renderRandomProducts();
   } else {
     imageSection.removeEventListener('click', handleProductClick)
-    renderProductData();
+    renderProductDataList();
+    renderProductDataChart();
   }
 };
 
 // create a function to render the results data to a list on the page
-let renderProductData = function () {
+let renderProductDataList = function () {
   for (let i = 0; i < allProducts.length; i++) {
     let newLi = document.createElement("li");
     newLi.innerText = `${allProducts[i].name} was shown ${allProducts[i].timesShown} times and clicked ${allProducts[i].clicks} times`;
     totalsList.appendChild(newLi);
   }
 };
+// create a function to render the chart with collected data
+let renderProductDataChart = function(){
+  for (let i = 0; i < allProducts.length; i++) {
+    productLabels.push(allProducts[i].name);
+    productTimesShown.push(allProducts[i].timesShown);
+    productClicks.push(allProducts[i].clicks);
+  }
+// adding a chart with chart js
+const labels = productLabels;
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Times Selected',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: productClicks,
+  },
+    {label: 'Times Shown',
+    backgroundColor: 'rgb(25, 99, 132)',
+    borderColor: 'rgb(25, 99, 132)',
+    data: productTimesShown,
+}
+]
+};
+const config = {
+  type: 'bar',
+  data,
+  options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'BusMall Product Survey Results'
+        },
+      },
+      responsive: true,
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true
+        }
+  }
+}
+}
+  let productDataChart = new Chart(
+    document.getElementById('productDataChart'),
+    config
+  );
+}
 
 // load page with random products
 renderRandomProducts();
 
 // create an event listener to handle clicks
 imageSection.addEventListener("click", handleProductClick);
+
+
+
+
